2225610f6b788a362bfe88cb523d49017504fca1
raju chaudhary <rajuchaudhary1360@gamil.com>
2024-08-23 09:10:47 +0530

    array and its methods

--- type.ts
+++ type.ts
@@ -22,17 +22,116 @@ console.log(`this is also a inference ${no}`)  */
 // Same for other values like boolean , symbol, undefined, null etc..
 
 // symbol are unique even if they have same description
+
+/* 
 const id : symbol =Symbol("idNumber48545");
 const id2 : symbol =Symbol("idNumber48545");
 
-console.log(id ===id2)// false
+console.log(id ===id2)// false     */
 
 
 
 
+//? Array 
 
 
-//?Arrays 
-const numbers:number=[1,2,3,4,5,6,7,8,9,10];
-console.log(numbers)
+//Array operations and methods
+let numbers:any[]=[1,2,3,4,5,6,7,8,9];
+//console.log(numbers)
+let numbers2:string[]=["one","two","three","four","five","six","seven","eight"];
+
+
+/*
+
+numbers.push(numbers2);
+console.log(numbers);
+
+console.log(numbers[9])   
+let a:any[]=numbers.concat(numbers2);
+console.log(a.length);    
+
+console.log(...numbers)          // we can separate numbers by separators operator
+
+const number:any[]=[1,2,[1,2,3],4,5,["1", "2", "3"]];
+console.log(number);
+console.log(number.flat(Infinity))  // merge all arrays
+
+console.log(Array.isArray(number))  
+let a :number=4;
+console.log(Array.isArray(a))        // check whether it was an array or not
+
+console.log(Array.from("Coder"));     // it will give an array   
+
+const arr:any[]=Array.from({length:10},(item,index)=>index +1);  
+console.log(arr)
+
+const arr2=Array.from({length:10},(item,index)=>index+2);
+console.log(arr2)
+
+const alphabet = Array.from({length:26},(item,index)=>String.fromCharCode(97+index));
+console.log(alphabet) 
+
+const upperLetter = Array.from({length:26},(item,index)=>String.fromCharCode(65+index));
+console.log(upperLetter)
+const nums= Array.from({length:20},(item,index)=>index+1);  
+console.log(nums)                      
+
+
+let one=1
+let two=2;
+let three =3;
+
+console.log(Array.of({one,two,three}))  
+
+let number4:number[]=[1,2,3,4];
+number4.push(5);
+
+console.log(number4)
+console.log(`${number4.pop()} it print the last value which is pop`)
+ let a=number4.pop()
+console.log(number4)
+console.log(`we can also stor the last value as here ${a}`);
+
+number4.unshift(5);  // add value at first position 
+console.log(number4)
+
+let b =number4.shift()  // it will remove first value from array
+
+console.log(b)
+console.log(number4)
+
+console.log(number4.includes(84));
+console.log(number4.join())// convert array to string 
+let c= number4.join()
+console.log(c)              
+
+ 
+
+let number5=[1,2,3,4,5];
+let slices= number5.slice(0,3);
+console.log(slices);
+console.log(number5);
+let splices= number5.splice(0,3);
+console.log(splices);
+console.log(`${number5} after splice`);
+console.log(number5);                        */
+
+
+//!array.map((element, index, array) => {
+  // Return the new value for the new array
+//});
+// map create a new array 
+
+let number6= [1,2,3,4,]
+number6=number6.map((element,index)=>element+1);
+console.log(number6);
+
+let newArray = number6.map((element,index)=>element*2);
+console.log(newArray);
+
+const obj:any= {a:1,b:2,c:3,d:4};
+const entries =Object.entries(obj);
+console.log(entries);
+
+console.log(entries)
 
